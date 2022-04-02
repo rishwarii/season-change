4f2f99928c6f04c20c25a9abec571e47ce291624
Rishwari Ranjan <rishwari.ranjan@nagarro.com>
2022-04-03 03:03:01 +0530

    SeasonChangesFin

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,37 @@
+import "./SeasonDisplay.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-const SeasonDisplay = () => {
-  return <div>This is seaspm</div>;
+const SeasonConfig = {
+  summer: {
+    text: "Let's hit the BEACH ",
+    iconName: "sun",
+  },
+
+  winter: {
+    text: "Burrr it's chillyy ",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = SeasonConfig[season]; //text, iconName
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errMsg: "" };
 
-    //initialize state
-    this.state = { lat: null, errMsg: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //we called setState
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errMsg: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMsg: err.message })
     );
   }
+  renderContent() {
+    if (this.state.errMsg && !this.state.lat) {
+      return <div>Error : {this.state.errMsg}</div>;
+    }
 
+    if (!this.state.errMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept Location Request" />;
+  }
   //react need us to define render method
   render() {
-    return (
-      <div>
-        Latitude:{this.state.lat}
-        <br />
-        Error: {this.state.errMsg}
-      </div>
-    );
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
